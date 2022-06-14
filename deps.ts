5749ffb9c8844c021d5de327e6f1428a3577f2a9
Alexiy Rybin <alexiy.rybin@gmail.com>
2022-06-14 16:59:59 +0700

    Updated std

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/x/harmony@v2.6.0/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-export { serve } from "https://deno.land/std@0.128.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.143.0/http/server.ts";
